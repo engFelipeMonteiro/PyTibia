a303b00e884622f6182b8ea0122f721718f7e2e2
Felipe Monteiro Jácome <fmj1988@gmail.com>
2023-06-03 17:36:17 -0300

    AJSUTES

--- src/utils/core.py
+++ src/utils/core.py
@@ -1,15 +1,19 @@
 import cv2
-import dxcam
+#import dxcam
+from src.utils.camera import PyCamera
 import numpy as np
 import pyautogui
 import random
 import time
+import imutils
 from typing import Callable, Union
 import xxhash
 from src.shared.typings import BBox, Coordinate, GrayImage, XYCoordinate
 
 
-camera = dxcam.create(output_color='GRAY')
+#camera = dxcam.create(output_color='GRAY')
+camera = PyCamera()
+
 
 
 # TODO: add unit tests
@@ -19,14 +23,14 @@ def cacheObjectPosition(func: Callable) -> Callable:
     lastW = None
     lastH = None
     lastImgHash = None
-    def inner(screenshot):
+    def inner(screenshot, **kws):
         nonlocal lastX, lastY, lastW, lastH, lastImgHash
         if lastX != None and lastY != None and lastW != None and lastH != None:
             copiedImg = screenshot[lastY:lastY + lastH, lastX:lastX + lastW]
             copiedImgHash = hashit(copiedImg)
             if copiedImgHash == lastImgHash:
                 return (lastX, lastY, lastW, lastH)
-        res = func(screenshot)
+        res = func(screenshot, **kws)
         didntMatch = res is None
         if didntMatch:
             return None
@@ -64,14 +68,25 @@ def hashitHex(arr: np.ndarray) -> str:
 
 
 # TODO: add unit tests
-def locate(compareImage: GrayImage, img: GrayImage, confidence: float=0.85) -> Union[BBox, None]:
+def locate(compareImage: GrayImage, img: GrayImage, confidence: float=0.85, is_first=True, scale:float=None) -> Union[BBox, None]:
+    # import cv2; match = cv2.matchTemplate(screenshot, images['arrows']['leftGameWindow11'], cv2.TM_CCOEFF_NORMED); cv2.minMaxLoc(match)
+    # import cv2; match = cv2.matchTemplate(screenshot, images['arrows']['rightGameWindow11'], cv2.TM_CCOEFF_NORMED); cv2.minMaxLoc(match) 
+    if scale and not is_first:
+        # try resize at second call
+        img = imutils.resize(img, width=int(img.shape[1]*scale))
     match = cv2.matchTemplate(compareImage, img, cv2.TM_CCOEFF_NORMED)
     res = cv2.minMaxLoc(match)
     matchConfidence = res[1]
     didntMatch = matchConfidence <= confidence
+    print(f"\nmatch confidence: {matchConfidence}")
     if didntMatch:
-        return None
+        if is_first:
+            # run again with diferent size
+            return locate(compareImage, img, is_first=False, scale=scale)
+        else:
+            return None
     (x, y) = res[3]
+    
     width = len(img[0])
     height = len(img)
     return x, y, width, height
@@ -79,7 +94,7 @@ def locate(compareImage: GrayImage, img: GrayImage, confidence: float=0.85) -> U
 
 # TODO: add unit tests
 def locateMultiple(compareImg: GrayImage, img: GrayImage, confidence: float=0.85) -> Union[BBox, None]:
-    match = cv2.matchTemplate(compareImg, img, cv2.TM_CCOEFF_NORMED)
+    match = cv2.matchTemplate(compareImg, img, cv2.TM_CCOEFF)
     loc = np.where(match >= confidence)
     resultList = []
     for pt in zip(*loc[::-1]):
@@ -98,6 +113,14 @@ def getScreenshot() -> GrayImage:
     screenshotReshaped = np.array(screenshot, dtype=np.uint8).reshape((screenshotHeight, screenshotWidth))
     return screenshotReshaped
 
+
+def getScreenshot_osx() -> GrayImage:
+    global camera
+    if not camera.is_capturing:
+        pass
+    screenshot = camera.get_latest_frame(test=True)
+
+    return screenshot
 
 # TODO: add unit tests
 def press(key: str, delay: int=150):
